4029ba100b2fd220b04708aa8985f82638a2eb11
justin199 <beallj8@gmail.com>
2019-01-22 15:20:11 +0000

    bottomed out income so you cant be in debt

--- PeopleSimulator/src/Main.java
+++ PeopleSimulator/src/Main.java
@@ -7,7 +7,7 @@ class Main {
 		Society peopleTown = new Society(1000);
 
       for(int i = 0; i < 1000; i++) {
-        peopleTown.cycleDay();
+        //peopleTown.cycleDay();
         //Write your print/tests in here if your feature should happen on a dialy basis
 
 
@@ -17,8 +17,8 @@ class Main {
       for(Person individual : peopleTown.people){
           //This is a test of the people who got married
           if(individual.getSpouse() != null){
-              System.out.println(individual.getName() + " married to " + individual.getSpouse().getName() );
-              System.out.println(individual.getSpouse().getName() + " is married to " + individual.getName());
+              //System.out.println(individual.getName() + " married to " + individual.getSpouse().getName() );
+              //System.out.println(individual.getSpouse().getName() + " is married to " + individual.getName());
           }
           //Write your print/tests below if they involve a property of the people in peopleTown
           //use individual as it does in the example above
